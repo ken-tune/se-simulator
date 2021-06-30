f519f18b05b69f27a06c2d19c30b160bc536427e
Ken Tune <ktune@aerospike.com>
2021-06-30 12:14:38 +0100

    Good commit

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t trade-data-gen -t ktune/se-simulator:tradeGen -f "../docker/Dockerfile-tradeGen" ../docker
-docker build -t trade-server -t ktune/se-simulator:trade-server -f "../docker/Dockerfile-tradeServer" ../docker
+docker build -t trade-store-server -t ktune/se-simulator:trade-store-server -f "../docker/Dockerfile-TradeStoreServer" ../docker
 
 docker push ktune/se-simulator:tradeGen
-docker push ktune/se-simulator:trade-server
+docker push ktune/se-simulator:trade-store-server
