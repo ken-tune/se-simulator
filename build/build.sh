9ce6e45e1931ede83f46bfd55d2c631f1738e414
Ken Tune <ktune@aerospike.com>
2021-07-06 17:49:04 +0100

    Force rebuild after code updates

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker build -t trade-data-gen -t ktune/se-simulator:tradeGen -f "../docker/Dockerfile-tradeGen" ../docker
-docker build -t trade-store-server -t ktune/se-simulator:trade-store-server -f "../docker/Dockerfile-TradeStoreServer" ../docker
+# Run trade-date-gen build with --no-cache to make sure we always get the latest code
+docker build --no-cache -t trade-data-gen -t ktune/se-simulator:tradeGen -f "../docker/Dockerfile-tradeGen" ../docker
+# Changing the BUILD_TIME argument forces a git clone every time
+docker build --build-arg BUILD_TIME=$(date +%Y%m%d-%H%M%S) -t trade-store-server -t ktune/se-simulator:trade-store-server -f "../docker/Dockerfile-TradeStoreServer" ../docker
 
 docker push ktune/se-simulator:tradeGen
 docker push ktune/se-simulator:trade-store-server
